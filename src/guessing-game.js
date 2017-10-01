45da7a0cd72a6fbda7df420f6f0b132be8c31ef2
FaizUndead <faiz2806@gmail.com>
2017-10-01 16:15:05 +0300

    Done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,36 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.ans = null;
+        this.min = null;
+        this.max = null;
+
+    }
+    _search() {
+        return Math.round((this.min + this.max) / 2);
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.ans =  this._search();
     }
 
     guess() {
-
+        if (this.min > this.max) {
+            return null;
+        } else {
+            return this.ans;
+        }
     }
 
     lower() {
-
+        this.max = this.ans;
+        this.ans =  this._search();
     }
 
     greater() {
-
+        this.min = this.ans;
+        this.ans = this._search();
     }
 }
 
